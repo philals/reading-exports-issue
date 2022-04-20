f9b58daaa1bd4a496b4bea10b32eec1c8b5ffeac
Phil Alsford <palsford@linz.govt.nz>
2022-04-20 13:57:25 +1200

    WIP

--- container/src/App.tsx
+++ container/src/App.tsx
@@ -1,13 +1,18 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { Suspense, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+const RemoteApp = React.lazy(() => import("home/Button"));
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <header className="App-header">
+        <Suspense fallback={"loading..."}>
+          <RemoteApp />
+        </Suspense>
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
         <p>
@@ -27,7 +32,7 @@ function App() {
           >
             Learn React
           </a>
-          {' | '}
+          {" | "}
           <a
             className="App-link"
             href="https://vitejs.dev/guide/features.html"
@@ -39,7 +44,7 @@ function App() {
         </p>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- header/vite.config.ts
+++ header/vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import federation from "@originjs/vite-plugin-federation";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  server: {
+    port: 3001,
+  },
+  preview: {
+    port: 3001,
+  },
+  plugins: [
+    react(),
+    federation({
+      name: "home",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./Button": "./src/App",
+      },
+      shared: { react: { singleton: true }, "react-dom": { singleton: true } },
+    }),
+  ],
+  build: {
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+  },
+});
